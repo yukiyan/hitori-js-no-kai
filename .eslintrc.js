b02f09580ddbbc76c5e67af5a5e4650343062287
yukiyan <wakayama0215@gmail.com>
2016-10-20 02:32:28 +0900

    :wrench: Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "rules": {
+    "no-console": 0,
+    "no-unused-vars": [2, { "argsIgnorePattern": "^_" }],
+    "import/newline-after-import": 0,
+  }
 };
