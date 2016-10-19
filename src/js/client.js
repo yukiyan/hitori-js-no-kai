d9b769e59f71735cd7cf79981a6edb86bc77f647
yukiyan <wakayama0215@gmail.com>
2016-10-20 03:43:50 +0900

    :shirt: Fix lint error

--- src/js/client.js
+++ src/js/client.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <h1>やっていく</h1>
-    );
-  }
-}
+const Layout = () => (
+  <h1>やっていく</h1>
+);
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(<Layout />, app);
