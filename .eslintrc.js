8198e7186098fc110e190546802607ff35fa6629
yukiyan <wakayama0215@gmail.com>
2016-10-20 03:42:57 +0900

    :wrench: Optimize eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,15 @@ module.exports = {
     "jsx-a11y",
     "import"
   ],
+  "env": {
+    "browser": true
+  },
   "rules": {
     "no-console": 0,
     "no-unused-vars": [2, { "argsIgnorePattern": "^_" }],
     "import/newline-after-import": 0,
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.jsx']
+    }],
   }
 };
