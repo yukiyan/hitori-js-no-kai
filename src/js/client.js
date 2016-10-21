558bb0febbc4135e4a13e1bd50acaff50a016890
yukiyan <wakayama0215@gmail.com>
2016-10-22 05:37:44 +0900

    :recycle: Refactor

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Layout = () => (
-  <h1>やっていく</h1>
+const Layout = () => <h1>やっていく</h1>;
+
+ReactDOM.render(
+  <Layout />,
+  document.getElementById('app')
 );
-
-const app = document.getElementById('app');
-
-ReactDOM.render(<Layout />, app);
