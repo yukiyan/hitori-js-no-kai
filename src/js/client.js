678a1784f2518782a5226b9fbd6af3d046c76087
yukiyan <wakayama0215@gmail.com>
2016-10-22 05:44:51 +0900

    :shirt: Fix `Prevent missing props validation`
    https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md

--- src/js/client.js
+++ src/js/client.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 const Layout = props => <h1>{props.name} がやっていく</h1>;
 
+Layout.propTypes = {
+  name: React.PropTypes.string.isRequired,
+};
+
 ReactDOM.render(
   <Layout name="yukiyan" />,
   document.getElementById('app')
