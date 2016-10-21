8f22af2f80d44df122008d43d49370a350b049b3
yukiyan <wakayama0215@gmail.com>
2016-10-22 05:48:50 +0900

    :+1: Use defaultProps

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Layout = props => <h1>{props.name} がやっていく</h1>;
+const Layout = props => <h1>{props.name} がやっていく {props.children}</h1>;
 
 Layout.propTypes = {
   name: React.PropTypes.string.isRequired,
+  children: React.PropTypes.string.isRequired,
+};
+
+Layout.defaultProps = {
+  children: 'これからずっとな',
 };
 
 ReactDOM.render(
