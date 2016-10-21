1cacd81efb9fb51a4296702e258ba5418f6fb700
yukiyan <wakayama0215@gmail.com>
2016-10-22 02:15:54 +0900

    :tada: Add .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-        }
+        loader: 'babel-loader'
       }
     ]
   },
