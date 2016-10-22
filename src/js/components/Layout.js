a75b10fb76a4cee8505b400c9b4988bfb73423a6
yukiyan <wakayama0215@gmail.com>
2016-10-22 20:41:40 +0900

    :eye: See a VirtualDOM's diffing rendering

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,11 +2,25 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = () => (
-  <div>
-    <Header />
-    <Footer />
-  </div>
-);
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'Yukiyan!!!' };
+  }
+
+  render() {
+    setTimeout(() => {
+      this.setState({ name: 'Wakayama' });
+    }, 2000);
+
+    return (
+      <div>
+        {this.state.name}
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Layout;
