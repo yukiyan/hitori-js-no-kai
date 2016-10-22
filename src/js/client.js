aa4e33d0220925ca6f4a30ff5213bf1f92b4f627
yukiyan <wakayama0215@gmail.com>
2016-10-22 19:31:16 +0900

    :wrench: Add basic component

--- src/js/client.js
+++ src/js/client.js
@@ -1,30 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="input your text"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
-  }
-}
+const Layout = () => (
+  <h1>It works!!</h1>
+);
 
 ReactDOM.render(
   <Layout />,
