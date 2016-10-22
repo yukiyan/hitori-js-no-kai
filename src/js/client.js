da6d767bcd6bbbc0afda935e58f414db2a7bc2b2
yukiyan <wakayama0215@gmail.com>
2016-10-22 19:52:56 +0900

    :wrench: Add some function

--- src/js/client.js
+++ src/js/client.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Layout = () => (
-  <h1>It works!!</h1>
-);
+
+class Layout extends React.Component {
+  static getVal(val) {
+    return `Will${val}`;
+  }
+
+  render() {
+    return (
+      <h1>It works!! {Layout.getVal(1)}!</h1>
+    );
+  }
+
+}
 
 ReactDOM.render(
   <Layout />,
