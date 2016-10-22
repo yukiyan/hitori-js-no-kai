825fdd13bc52c28f70451d61650e334ef8e8f753
yukiyan <wakayama0215@gmail.com>
2016-10-22 20:23:12 +0900

    Revert ":sparkles: Try list"
    This reverts commit b4aefb8c3c0335096216cb4f7496431653b4a208.

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import Header from './Header';
 
-const list = [
-  <Header />,
-  <Header />,
-  <Header />,
-];
-
 const Layout = () => (
   <div>
-    {list}
+    <Header />
   </div>
 );
 
